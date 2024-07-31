961786da09ff75a5fbefcd8647b8b07e9d910410
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2024-07-31 21:14:37 +0330

    implement/ initializing the project

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        lightBackground: "#FAFAFA",
+        primary: "#FFFFFF",
+        secondary: "#F6F7F9",
+      },
+      colors: {
+        bluePrimary: "#1043A6",
+        blueSecondary: "#1043A6",
+        darkPrimary: "#313235",
+        darkSecondary: "#7E838F",
+        lightPrimary: "#F9F5FF",
+      },
+      borderColor: {
+        primary: "#EAECF0",
+      },
+      fontSize: {
+        sm: "14px",
+        md: "16px",
+      },
+      fontFamily: {
+        IranSans: "IRANSansWeb",
+      },
+    },
+  },
+  plugins: [],
+};
