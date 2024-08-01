ebc0ccfcf3be8c39fcf6c42ae944cfbbd4016e21
Sina Haghshenas <103804601+Sina-Hgs@users.noreply.github.com>
2024-08-01 17:46:06 +0330

    implement/ added a super simple design to the error page

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,5 +1,9 @@
 const Error = () => {
-  return <div>Something went wrong. Please Try Again!</div>;
+  return (
+    <div className="absolute w-full flex justify-center top-12 text-xl text-bluePrimary">
+      Something went wrong. Please Try Again!
+    </div>
+  );
 };
 
 export default Error;
